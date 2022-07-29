58992c5df4a9752182dc78b24db8c7e2fa4feb86
chaojunyang <chaojunyang@yunify.com>
2022-07-29 08:11:35 +0800

    feat: 新增Nacos配置中心

--- order-service/src/main/java/com/ycj/order/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/ycj/order/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ycj.order.service.impl;
 
+import com.ycj.order.clients.UserClient;
 import com.ycj.order.mapper.OrderMapper;
 import com.ycj.order.pojo.Order;
 import com.ycj.order.pojo.User;
@@ -17,16 +18,29 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private UserClient userClient;
+
     @Override
     public Order queryById(Long id){
+//        // 查看订单
+//        Order order = orderMapper.queryById(id);
+//        // 2. 利用RestTemplate发起http请求，查询用户
+//        // 2.1 url路径
+//        // String url = "http://localhost:8081/user/" + order.getUserId();
+//        String url = "http://userservice/user/" + order.getUserId();
+//        // 2.2 发送http请求，实现远程调用
+//        User user = restTemplate.getForObject(url, User.class);
+//        // 3. 封装到user到order
+//        order.setUser(user);
+//        // 4. 返回
+//        return order;
+
         // 查看订单
         Order order = orderMapper.queryById(id);
-        // 2. 利用RestTemplate发起http请求，查询用户
-        // 2.1 url路径
-        String url = "http://localhost:8081/user/" + order.getUserId();
-        // 2.2 发送http请求，实现远程调用
-        User user = restTemplate.getForObject(url, User.class);
-        // 3. 封装到user到order
+        // 2. 用Feign远程调用
+        User user = userClient.findById(order.getUserId());
+        // 3. 封装user到Order
         order.setUser(user);
         // 4. 返回
         return order;
--- user-service/src/main/java/com/ycj/user/controller/UserController.java
+++ user-service/src/main/java/com/ycj/user/controller/UserController.java
@@ -1,19 +1,42 @@
 package com.ycj.user.controller;
 
+import com.ycj.user.config.PatternProperties;
 import com.ycj.user.pojo.User;
 import com.ycj.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 @RequestMapping("user")
+//@RefreshScope
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+//    @Value("${pattern.dateformat}")
+//    private String dateformat;
+
+    @Autowired
+    private PatternProperties properties;
+
+    @GetMapping("now")
+    public String now() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern(properties.getDateformat()));
+    }
+
+    @GetMapping("prop")
+    public PatternProperties properties() {
+        return properties;
+    }
 
     @GetMapping("{id}")
     public User queryById(@PathVariable("id") Long id) {
